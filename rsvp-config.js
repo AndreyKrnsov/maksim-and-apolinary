9d7b0a6ca5c4b9fc68e2ab3caaa1d0bb8e35a9f5
akurnosov <andrkurnosov4@gmail.com>
2026-05-28 01:32:38 +0400

    Connect RSVP form to Google Forms

--- rsvp-config.js
+++ rsvp-config.js
@@ -1,13 +1,13 @@
 window.RSVP_CONFIG = {
   endpoint: "",
-  googleFormAction: "",
+  googleFormAction: "https://docs.google.com/forms/d/e/1FAIpQLSfv76eSD2kxc6g_aEaVMKR2j3DTydh4HvMLHjL3MdjrvbOlHg/formResponse",
   googleFormFields: {
-    guestName: "",
-    attendance: "",
-    overnight: "",
-    alcohol: "",
-    transfer: "",
-    invitationFor: "",
-    submittedAt: "",
+    guestName: "entry.999175955",
+    attendance: "entry.910686136",
+    overnight: "entry.2047679745",
+    alcohol: "entry.601704273",
+    transfer: "entry.518522608",
+    invitationFor: "entry.1431106606",
+    submittedAt: "entry.2016748523",
   },
 };
